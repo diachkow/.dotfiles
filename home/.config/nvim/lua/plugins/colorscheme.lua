55b62617431546894f8b698b472d0c4d92fe8b62
diachkow <diachkovspecial@gmail.com>
2026-03-07 19:17:45 +0200

    feat: change colorscheme to gruvbox-material

--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,12 @@
 return {
   {
-    "Everblush/nvim",
-    name = "everblush",
+    "sainnhe/gruvbox-material",
+    name = "gruvbox-material",
     config = function()
-      require("everblush").setup({
-        override = {
-          MiniStatuslineModeNormal = { fg = "#141b1e", bg = "#67b0e8", bold = true },
-          MiniStatuslineModeInsert = { fg = "#141b1e", bg = "#8ccf7e", bold = true },
-          MiniStatuslineModeVisual = { fg = "#141b1e", bg = "#c47fd5", bold = true },
-          MiniStatuslineModeReplace = { fg = "#141b1e", bg = "#e57474", bold = true },
-          MiniStatuslineModeCommand = { fg = "#141b1e", bg = "#e5c76b", bold = true },
-          MiniStatuslineModeOther = { fg = "#141b1e", bg = "#6cbfbf", bold = true },
-          MiniStatuslineDevinfo = { fg = "#dadada", bg = "#232a2d" },
-          MiniStatuslineFilename = { fg = "#8ccf7e", bg = "#141b1e" },
-          MiniStatuslineFileinfo = { fg = "#dadada", bg = "#232a2d" },
-          MiniStatuslineInactive = { fg = "#dadada", bg = "#141b1e" },
-        },
-      })
-      vim.cmd.colorscheme("everblush")
+      vim.g.gruvbox_material_background = "medium"
+      vim.g.gruvbox_material_foreground = "mix"
+      vim.o.background = "dark"
+      vim.cmd.colorscheme("gruvbox-material")
     end,
   },
 }
