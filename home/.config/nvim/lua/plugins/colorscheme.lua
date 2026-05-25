0ecd25a4845476cdec01ff40f6728dd9ade6575f
diachkow <diachkovspecial@gmail.com>
2026-05-25 12:50:03 +0300

    feat: remove italic font from code rendering

--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,21 @@ return {
     config = function()
       require("catppuccin").setup({
         flavour = "mocha",
+        no_italic = true,
+        styles = {
+          comments = {},
+          conditionals = {},
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+        },
       })
       vim.o.background = "dark"
       vim.cmd.colorscheme("catppuccin")
